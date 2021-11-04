8c6bd760b2456d9b650096f70a106e2b32dba5d9
pirozzzi <davidepiria@live.it>
2021-11-04 17:15:12 +0100

    s

---
+++ max.c
@@ -0,0 +1,7 @@
+#include "max.h"
+int max(a,b){
+    if(a>b)
+    return a;
+    else
+    return b;
+}
--- top.c
+++ top.c
@@ -1,17 +1,9 @@
 #include<stdio.h>
-#include<math.h>
-int max(int a, int b);
+#include "max.h"
 int main(){
     int a,b;
     printf("Inserisci due numeri: \n");
     scanf("%d",&a);
     scanf("%d",&b);
     printf("Il maggiore tra i due e': %d",max(a,b));
-
-}
-int max(a,b){
-    if(a>b)
-    return a;
-    else
-    return b;
 }
